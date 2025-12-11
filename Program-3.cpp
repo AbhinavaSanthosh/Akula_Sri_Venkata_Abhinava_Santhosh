df6bdcc7c5261a2d9461c3d584c0189213d967f4
AKULA SRI VENKATA ABHINAVA SANTHOSH <140000489+AbhinavaSanthosh@users.noreply.github.com>
2025-12-11 22:36:46 +0530

    Implement series of odd numbers based on user input
    This program prompts the user for the length of a series and prints the first 'x' odd numbers.

---
+++ Program-3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  cout<<"Enter the Value of length of series: ";
+  int x;
+  cin>>x;
+  if(x%2 == 0){
+    x--;
+  }
+  int cnt = 0;
+  for(int i = 1; i<= 2*x; ++i){
+      if(i%2){
+        cout<<i<<" ";
+        cnt++;
+      }
+    if(cnt == x)
+        break;
+  }
+  return 0;
+}
